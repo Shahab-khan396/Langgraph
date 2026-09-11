8b4641e144c1187a73d1b6d6aa04a50756193153
Shahab-khan396 <ksab2922@gmail.com>
2026-09-11 20:13:24 +0500

    Add colorama dependency and implement initial graph state structure

---
+++ Langgraph_toturial/module_1/0_graph_state_str.py
@@ -0,0 +1,34 @@
+# import libraries 
+
+from langgraph.graph import StateGraph, START, END
+
+# state
+
+
+# node 
+def greeting(state:str)-> str:
+      """ this is a simple greeting function """
+      return state + " welcome to Langgraph!"
+
+
+
+
+# edges
+
+
+
+# langgraph putting our graph together
+
+builder = StateGraph(str)
+
+# add nodes to the graph
+builder.add_node("greeting", greeting)
+
+
+# add edges to the graph
+builder.add_edge(START, "greeting")
+builder.add_edge("greeting", END)
+
+graph= builder.compile()
+
+print(graph.invoke("Hi! i am excited to learn about Langgraph. "))
